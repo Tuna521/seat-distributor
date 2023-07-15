4624a2fa1ce9d632edda35cdbab4a0840d17104b
Tina Wang <wangzongting521@gmail.com>
2023-07-15 14:16:11 +0100

    Split the correct number of tickets to the customer

--- distributor.py
+++ distributor.py
@@ -14,28 +14,33 @@ num_customer_header = len(customer_info_header)
 i_name = -1
 i_surname = -1
 i_quantity = -1
-cur_i = 0
+i = 0
 
-while cur_i < num_customer_header:
-    if (customer_info_header[cur_i] == "First Name"):
-        i_name = cur_i
-    if (customer_info_header[cur_i] == "Surname"):
-        i_surname = cur_i
-    if (customer_info_header[cur_i] == "Quantity"):
-        i_quantity = cur_i
-    cur_i += 1
+while i < num_customer_header:
+    if (customer_info_header[i] == "First Name"):
+        i_name = i
+    if (customer_info_header[i] == "Surname"):
+        i_surname = i
+    if (customer_info_header[i] == "Quantity"):
+        i_quantity = i
+    i += 1
 
 if i_name == -1:
     raise Exception("csv does not contain \"First Name\" column")
 if i_quantity == -1:
-    raise Exception("csv does not contain \"First Name\" column")
+    raise Exception("csv does not contain \"Quantity\" column")
 
 # Assign pdf and attach the name at the end
 # (DOESN'T ACCOUNT THAT TWO TICKETS NEXT TO EACH OTHER (yet))
-for i in range(len(inputpdf.pages)):
-    customer_info = next(customer_info_reader)
+i = 0
+for customer_info in customer_info_reader:
+    print(i)
+    for n in range(int(customer_info[i_quantity])):
+        output = PdfWriter()
+        output.add_page(inputpdf.pages[i])
+        with open("document-%s-%s.pdf" % (i, customer_info[i_name]), "wb") as outputStream:
+            output.write(outputStream)
+        i += 1
 
-    output = PdfWriter()
-    output.add_page(inputpdf.pages[i])
-    with open("document-%s-%s.pdf" % (i, customer_info[i_name]), "wb") as outputStream:
-        output.write(outputStream)
+# check whehter the number of ticket split is the same
+# len(inputpdf.pages)
