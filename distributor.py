292442e5dac9e4a9aa874b96e2e8d587ae5aa9aa
Tina Wang <wangzongting521@gmail.com>
2023-07-16 23:12:15 +0100

    Separate tickets by the seat number

--- distributor.py
+++ distributor.py
@@ -1,6 +1,10 @@
 from PyPDF2 import PdfWriter, PdfReader
 from csv import reader
 
+# The number of rows and seats [included] (ex P16-25, Q17-Q21)
+seat_row = ["P", "Q"]
+seat_range = [(16, 25), (17, 21)]
+
 # Open the pdf file of the collective tickets
 inputpdf = PdfReader(open("mamma-mia.pdf", "rb"))
 
@@ -33,14 +37,23 @@ if i_quantity == -1:
 # Assign pdf and attach the name at the end
 # (DOESN'T ACCOUNT THAT TWO TICKETS NEXT TO EACH OTHER (yet))
 i = 0
+
+cur_row_i = 0
+cur_seat_num = seat_range[cur_row_i][0]
 for customer_info in customer_info_reader:
     print(i)
     for n in range(int(customer_info[i_quantity])):
         output = PdfWriter()
         output.add_page(inputpdf.pages[i])
-        with open("document-%s-%s.pdf" % (i, customer_info[i_name]), "wb") as outputStream:
+        with open("document-%s%s-%s.pdf" % (seat_row[cur_row_i], cur_seat_num, customer_info[i_name]), "wb") as outputStream:
             output.write(outputStream)
         i += 1
+        
+        # Get next seat
+        cur_seat_num += 1
+        if (cur_seat_num > seat_range[cur_row_i][1]):
+            cur_row_i += 1
+            cur_seat_num = seat_row[cur_row_i][0]
 
 # check whehter the number of ticket split is the same
 # len(inputpdf.pages)
